aa31b82e7ea2cd2f04412ba3bfe34b3b82dc08f9
Blaussh <shai.gm@gmail.com>
2018-02-06 08:25:55 +0200

    Added helloWorldNodeJs project and Profile App project to solution. wrote the Profile App till the point of printing one hardcoded user's details

---
+++ HelloWorldNodeJs/HelloWorldNodeJs/app.js
@@ -0,0 +1,7 @@
+'use strict';
+const name = "Andrew";
+const details = { favouriteLanguage: "JavaScript", age: 33, children: 3 }
+const errorMessage = "Something bad has occurred";
+console.log(name);
+console.dir(details);
+console.error(errorMessage);
