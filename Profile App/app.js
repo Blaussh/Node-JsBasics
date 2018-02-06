4480f04051087a71960b40dc26fbfc03ee6f2b95
Blaussh <shai.gm@gmail.com>
2018-02-06 08:48:10 +0200

    Changed the code to handle args

--- Profile App/app.js
+++ Profile App/app.js
@@ -3,28 +3,40 @@
 
 //Require https module
 const https = require("https");
-const username = "chalkers";
 //Function to print message to consose
 function printMessage(username, badgeCount, points) {
     const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
     console.log(message);
 }
 
-//Connect to the API URL (https://teamtreehouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`,
-    response => {
-        let body = "";
-        //Read the data
-        response.on('data', data => {
-            body += data.toString();
-        });
+function getProfile(username) {
+    //Connect to the API URL (https://teamtreehouse.com/username.json)
+    const request = https.get(`https://teamtreehouse.com/${username}.json`,
+        response => {
+            let body = "";
+            //Read the data
+            response.on('data', data => {
+                body += data.toString();
+            });
 
-        response.on('end', () => {
-            //Parse the data
-            const profile = JSON.parse(body);
-            //console.dir(profile);
-            printMessage(username, profile.badges.length, profile.points.JavaScript);
-            //Print the data
+            response.on('end', () => {
+                //Parse the data
+                const profile = JSON.parse(body);
+                //console.dir(profile);
+                printMessage(username, profile.badges.length, profile.points.JavaScript);
+                //Print the data
+            });
         });
-    });
+}
+
+const users = process.argv.slice(2);
+users.forEach(getProfile);
+//const users = ["chalkers", "alenaholligan","shaiblaustien","davemcfarland"];
+
+//This
+//users.forEach(getProfile);
+
+//Equles to:
+//users.forEach(username =>
+//    getProfile(username));
 
